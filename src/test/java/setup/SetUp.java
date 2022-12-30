c3817623cc4a73389db453bbb0000a1d03bae418
steduq10 <steven.duque.95@gmail.com>
2022-12-30 11:29:53 -0500

    configuracion inicial

---
+++ src/test/java/setup/SetUp.java
@@ -0,0 +1,37 @@
+package setup;
+
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+import net.thucydides.core.annotations.Managed;
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+
+import static com.google.common.base.StandardSystemProperty.USER_DIR;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+import static utils.Log4jValues.LOG4J_PROPERTIES_FILE_PATH;
+
+public class SetUp {
+    @Managed()
+    protected WebDriver browser;
+
+    protected void actorSetupTheBrowser(String actorName){
+        setUpLog4j2();
+        setupBrowser(browser);
+        setupUser(actorName,browser);
+    }
+
+    private void setupBrowser(WebDriver browser){
+        browser.manage().deleteAllCookies();
+        browser.manage().window().maximize();
+    }
+
+    private void setUpLog4j2(){
+        PropertyConfigurator.configure(USER_DIR.value() + LOG4J_PROPERTIES_FILE_PATH.getValue());
+    }
+
+    private void setupUser(String name, WebDriver driver){
+        OnStage.setTheStage(new OnlineCast());
+        theActorCalled(name).can(BrowseTheWeb.with(driver));
+    }
+}
